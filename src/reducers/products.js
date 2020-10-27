40163801d6b8b3248aa6c068914d7732b1c544c6
imhilla <hillaryodhiambo282@gmail.com>
2020-10-27 12:15:11 +0300

    add products reducer

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,3 +1,30 @@
+const initialState = {
+  filter: 'All',
+  books: [
+    { id: 1, title: 'Mummies', category: 'Horror' },
+    { id: 2, title: 'The Flash', category: 'Sci-Fi' },
+  ],
+};
+
+function books(state = initialState, action) {
+  switch (action.type) {
+    case 'CHANGE_FILTER':
+      return {
+        ...state,
+        filter: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+
+
+
+
+
+
+
 import axios from 'axios';
 
 function getData() {
