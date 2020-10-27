0d9286b08f06e40b65de48b81e5b1cd32a86d678
imhilla <hillaryodhiambo282@gmail.com>
2020-10-27 12:17:34 +0300

    log initial state products

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,12 +1,12 @@
 const initialState = {
-  filter: 'All',
-  books: [
+  // filter: 'All',
+  products: [
     { id: 1, title: 'Mummies', category: 'Horror' },
     { id: 2, title: 'The Flash', category: 'Sci-Fi' },
   ],
 };
 
-function books(state = initialState, action) {
+function products(state = initialState, action) {
   switch (action.type) {
     case 'CHANGE_FILTER':
       return {
@@ -18,31 +18,29 @@ function books(state = initialState, action) {
   }
 }
 
-
-
-
-
-
-
-
-import axios from 'axios';
-
-function getData() {
-  axios
-    .all([
-      axios.get('https://pokeapi.co/api/v2/ability/?limit=20&offset=20')
-    ])
-    .then(axios.spread((products) => showOutput(products)))
-    .catch(err => console.error(err));
-}
-
-getData()
-
-function showOutput(res) {
-  console.log(res)
-}
-
-function products(state = [], action) {
-  console.log('new')
-}
 export default products;
+
+
+
+
+
+// import axios from 'axios';
+
+// function getData() {
+//   axios
+//     .all([
+//       axios.get('https://pokeapi.co/api/v2/ability/?limit=20&offset=20')
+//     ])
+//     .then(axios.spread((products) => showOutput(products)))
+//     .catch(err => console.error(err));
+// }
+
+// getData()
+
+// function showOutput(res) {
+//   console.log(res)
+// }
+
+// function products(state = [], action) {
+//   console.log('new')
+// }
