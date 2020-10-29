db9eb2ea3105ff3de18091244fc1d9820fb69a2e
imhilla <hillaryodhiambo282@gmail.com>
2020-10-29 14:59:54 +0300

    refactor code

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -1,17 +1,22 @@
 import React from 'react';
-
 class Pokemon extends React.Component {
   render() {
     let ourData = this.props.data.products
+    console.log(ourData)
     return (
-      <div>
-        { Object.entries(ourData).forEach(([key, value]) => {
-          console.log(value)
-        })
+      <div className="pokeContainer">
+      <h2>hhcxc</h2>
+        { Object.entries(ourData).forEach(([key, value]) => (
+          <div>
+            <div>
+              <h2>{value.name}</h2>
+              <h2>{value.id}</h2>
+              <h1>Hello wwwww</h1>
+              {console.log(value.id)}
+            </div>
+          </div>
+        ))
         }
-        <h2>{this.props.name}!</h2>
-        <h3>Child Component</h3>
-        <h3>Location: {this.props.location}</h3>
       </div>
 
     );
@@ -19,34 +24,4 @@ class Pokemon extends React.Component {
 }
 
 
-// function renderPokemon(pokeData) {
-//   return (
-//     <div className="pokeContainer">
-//       <div className="pokeName">
-//         <h4>
-//           {pokeData.name}
-//         </h4>
-//         <p className="pokeNumber">{pokeData.id}</p>
-//         <ul className="pokeTypes">
-//           {createTypes(pokeData.types)}
-//         </ul>
-//       </div>
-//     </div>
-//   )
-// }
-
-// function createTypes(types) {
-//   types.forEach(function (type) {
-//     <li>{type['type']['name']}</li>
-//   })
-// }
-
-
-// function createTypes(types) {
-//   console.log(types)
-//   types.forEach(function (type) {
-//     <li>{type['type']['name']}</li>
-//   })
-// }
-
-export default Pokemon;
+export default Pokemon
