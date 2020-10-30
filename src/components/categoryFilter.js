2a6412a27a6d3bd5f28244ff68102b0fedb5c16c
imhilla <hillaryodhiambo282@gmail.com>
2020-10-30 18:00:24 +0300

    add uuid4 package and category filter function

---
+++ src/components/categoryFilter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const categories = ["CATEGORIES", 'poison', 'fire', 'water', 'bug', 'normal', 'electric', 'ground', 'grass', 'psychic', 'rock', 'fighting'];
+const renderCategories = categories.map(item => (
+  <option key={uuidv4()} value={item}>{item}</option>
+));
+
+const categoryFilter = ({ filter = 'All', handleFilterChange }) => (
+  <div className="panel-bg">
+    <div className="second-panel">
+      <div className="categories">
+        <select
+          id="inputGroupSelect01"
+          value={filter}
+          onChange={handleFilterChange}
+          className="CATEGORIES"
+        >
+          {renderCategories}
+        </select>
+      </div>
+    </div>
+  </div>
+);
+
+export default categoryFilter;
