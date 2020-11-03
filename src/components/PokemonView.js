051f2baf369198a3f7cebbe6b2fc68e1c15d34fe
imhilla <hillaryodhiambo282@gmail.com>
2020-11-03 19:37:47 +0300

    add pokemon conditional

--- src/components/PokemonView.js
+++ src/components/PokemonView.js
@@ -14,7 +14,6 @@ class PokemonView extends React.Component {
   render() {
     return (
       <div>
-        {/* <div>{productData}</div> */}
         <div>{this.state.id}</div>
       </div>
     );
--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -3,19 +3,17 @@ import { Link, Route } from "react-router-dom";
 // import PokemonView from './PokemonView'
 import { v4 as uuidv4 } from 'uuid';
 
-
 class Pokemon extends React.Component {
   constructor(props) {
     super(props);
     this.state = { pokemons: "empty" };
-    this.state = { clicked: false }
   }
 
-  // componentDidMount() {
-  //   setTimeout(() => {
-  //     this.setState({ products: this.props.data.products })
-  //   }, 3000)
-  // }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ pokemons: this.props.data.products })
+    }, 2000)
+  }
 
   render() {
     let ourData = this.props.data.products
@@ -30,27 +28,33 @@ class Pokemon extends React.Component {
         }
       })
     })
+
+    const PokemonList = ourData.length === 50 ? (
+      <div className="pokeContainer">
+        {myNewData.map((item, index) => (
+          <div className="container-div" key={index}>
+            <div className="img-container">
+              <Link to={`/pokemon/${item.id}`}>
+                <img src={`https://pokeres.bastionbot.org/images/pokemon/${item.id}.png`} alt="" />
+              </Link>
+            </div>
+            <h2 className="item-name">{item.name}</h2>
+            <p>#{item.id}</p>
+            {
+              item.types.map(type => (
+                <li>{type['type']['name']}</li>
+              ))
+            }
+          </div>
+        ))}
+      </div>) : (
+        <div>No pokemons yet</div>
+      )
+
     return (
       <div>
-        <div className="pokeContainer"
-        >
-          {myNewData.map((item, index) => (
-            <div className="container-div" key={index}>
-              <div className="img-container">
-                <Link to={`/pokemon/${item.id}`}>
-                  <img src={`https://pokeres.bastionbot.org/images/pokemon/${item.id}.png`} alt="" />
-                </Link>
-              </div>
-              <h2 className="item-name">{item.name}</h2>
-              <p>#{item.id}</p>
-              {
-                item.types.map(type => (
-                  <li>{type['type']['name']}</li>
-                ))
-              }
-            </div>
-          ))
-          }</div>
+        {PokemonList}
+        <div>Loading.......</div>
       </div>
     );
   }
