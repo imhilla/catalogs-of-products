6aad0715e75d428a3f3dcb78c3d8006d7e643a52
imhilla <hillaryodhiambo282@gmail.com>
2020-11-04 07:18:53 +0300

    add single page name feature

--- src/components/PokemonView.js
+++ src/components/PokemonView.js
@@ -7,7 +7,6 @@ class PokemonView extends React.Component {
   }
   componentDidMount() {
     let id = this.props.match.params.pokemon_id
-    let data
     let pokeData = axios(
       'https://pokeapi.co/api/v2/pokemon?limit=50',
     ).then(res => { this.setState({ data: res.data.results }) });
@@ -17,11 +16,23 @@ class PokemonView extends React.Component {
   }
 
   render() {
-    console.log(this.state.data);
+    const singlePoke = this.state.data !== undefined ? (
+      <div>
+        {this.state.data.map(item => {
+          if (item.url === `https://pokeapi.co/api/v2/pokemon/${this.state.id}/`) {
+            return (<h2>My name is {item.name}</h2>)
+          }
+        })
+        }
+        <div className="single-img">
+          <img src={`https://pokeres.bastionbot.org/images/pokemon/${this.state.id}.png`} alt="" />
+        </div>
+      </div>) : (
+        <div>Loading...</div>);
+
     return (
       <div>
-        <div>{this.state.id}</div>
-        <img src={`https://pokeres.bastionbot.org/images/pokemon/${this.state.id}.png`} alt="" />
+        {singlePoke}
       </div>
     );
   }
