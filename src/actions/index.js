485f85b5d250d511356830c9095130ed8afcb9d6
imhilla <hillaryodhiambo282@gmail.com>
2020-11-05 15:24:50 +0300

    fix change filter

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export function changeFilter(category) {
-  return { type: 'CHANGE_FILTER', payload: category };
-}
+export const changeFilter = category => ({ type: 'CHANGE_FILTER', payload: category });
