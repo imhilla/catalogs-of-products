d015578ee9503a5247217c4d615913f190d4d893
imhilla <hillaryodhiambo282@gmail.com>
2020-11-05 08:04:00 +0300

    fix pokemon linters

--- src/containers/pokemon.js
+++ src/containers/pokemon.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import PropTypes from 'prop-types';
 
 class Pokemon extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      render: false,
-    };
-    this.isMounted = false;
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     render: false,
+  //   };
+  //   this.isMounted = false;
+  // }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ render: true });
-    }, 3000);
-    this.isMounted = true;
-  }
+  // componentDidMount() {
+  // }
 
   componentWillUnmount() {
     this.isMounted = false;
   }
 
   render() {
-    const ourData = this.props.data.products;
-    const category = this.props.filter;
+    const { data } = this.props;
+    const ourData = data.products;
+    const category = data.filter;
     let myNewData = [];
     ourData.map(item => {
       item.types.map(type => {
@@ -34,12 +31,14 @@ class Pokemon extends React.Component {
         } else if (category === 'All' || category === 'CATEGORIES') {
           myNewData = ourData;
         }
+        return null;
       });
+      return null;
     });
 
     const PokemonList = ourData.length === 50 ? (
       <div className="pokeContainer">
-        {myNewData.map((item, index) => (
+        {myNewData.map(item => (
           <div className="container-div" key={uuidv4()}>
             <h2 className="item-name">{item.name}</h2>
             <div className="img-container">
@@ -57,14 +56,7 @@ class Pokemon extends React.Component {
           </div>
         ))}
       </div>
-    ) : (
-        <div>No pokemons yet, check your internet</div>
-      );
-
-    const renderContainer = false; // By default don't render anything
-    if (this.state.render) { // If this.state.render == true, which is set to true by the timer.
-      console.log('wait');
-    }
+    ) : (<div>No pokemons yet, check your internet</div>);
 
     return (
       <div className="pokelist">
@@ -75,11 +67,20 @@ class Pokemon extends React.Component {
 }
 
 Pokemon.propTypes = {
+  products: PropTypes.objectOf(PropTypes.string),
+  data: PropTypes.objectOf(PropTypes.any),
+  length: PropTypes.number,
   match: PropTypes.shape({
     params: PropTypes.shape({
       id: PropTypes.node,
     }).isRequired,
   }).isRequired,
+};
+
+Pokemon.defaultProps = {
+  products: {},
+  length: 50,
+  data: {},
 };
 
 export default Pokemon;
