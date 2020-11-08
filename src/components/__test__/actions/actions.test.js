f9f87d26e9fc0f6e81be83ae5de65b05fb382cce
imhilla <hillaryodhiambo282@gmail.com>
2020-11-08 10:57:34 +0300

    install axios mock adapter

--- src/components/__test__/actions/actions.test.js
+++ src/components/__test__/actions/actions.test.js
@@ -1,10 +1,16 @@
-// describe('gator_actions', () => {
-//   beforeEach(() => {
-//     store.clearActions();
-//   });
-//   describe('eatFood', () => {
-//     test('Dispatches the correct action and payload', () => {
-//       store.dispatch(gatorActions.eatFood('fish'));
-//       expect(store.getActions()).toMatchSnapshot();
-//     });
-//   });
+/* eslint-disable no-unused-vars */
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import { changeFilter } from '../../../actions/index';
+import '../../../setupTests';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const store = mockStore({});
+const mock = new MockAdapter(axios);
+
+beforeEach(() => {
+  store.clearActions();
+});
