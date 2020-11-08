6f2b098b03f29ec9602c3f9e9869d8da151b70c8
imhilla <hillaryodhiambo282@gmail.com>
2020-11-08 11:26:31 +0300

    refactor

--- src/components/__test__/actions/actions.test.js
+++ src/components/__test__/actions/actions.test.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-unused-vars */
-import MockAdapter from 'axios-mock-adapter';
-import axios from 'axios';
 import thunk from 'redux-thunk';
 import configureMockStore from 'redux-mock-store';
 import { changeFilter } from '../../../actions/index';
@@ -9,7 +6,6 @@ import '../../../setupTests';
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 const store = mockStore({});
-const mock = new MockAdapter(axios);
 
 beforeEach(() => {
   store.clearActions();
